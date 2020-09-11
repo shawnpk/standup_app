4cc62b05181348b8da9db6785cb6948958da2f05
Shawn Kearney <shawnpk29@me.com>
2020-09-11 13:19:43 -0600

    Generate activity controller

--- spec/views/activity/feed.html.erb_spec.rb
+++ spec/views/activity/feed.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "activity/feed.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'renders the word feed' do
+    render template: 'activity/feed.html.erb'
+    expect(rendered).to match(/feed/)
+  end
 end
